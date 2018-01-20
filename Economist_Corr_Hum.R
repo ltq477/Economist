78c897964c35429502cc9d7524ad58d932f034d1
Lawrence Quesada <ltquesada@gmail.com>
2018-01-20 00:32:40 -0800

    Added comments to code

--- Economist_Corr_Hum.R
+++ Economist_Corr_Hum.R
@@ -15,23 +15,19 @@ plt <- ggplot(df, aes(CPI, HDI))
 
 #Add geom_point
 plt + geom_point(aes(color=Region),  size=4.3, shape='O') + 
-  #Add trend line
+  #Add Trend Line
   geom_smooth(method = 'lm', formula = y ~ log(x), se= FALSE, color='red') + 
-  #set Coordinates
-  coord_cartesian(xlim=c(1,10), ylim=c(0.2,1)) + 
-  #Label X Axis and Y Axis
-  xlab('Corruption Perception Index, 2011 (10=Least Corrupt)') + 
-  ylab('Human Development Index, 2011 (1=Best)') + 
-  #Country Labels
+  #Add Scale and label to x axis
+  scale_x_continuous(name = "Corruption Perceptions Index, 2011 (10=least corrupt)", limits = c(.9, 10.5), breaks=1:10) + 
+  #Add Scale and label to y axis
+  scale_y_continuous(name = "Human Development Index, 2011 (1=Best)", limits = c(0.2, 1.0)) +
+  #Label points for specific countries
   geom_text(aes(label=Country), color='gray', data = subset(df, Country %in% label.points), 
-            check_overlap = TRUE, alpha=1) + 
-  #Add theme
-  theme_bw() + 
-  #set theme to center
-  theme(plot.title = element_text(hjust=0.5)) + 
-  #Add Chart Title
-  ggtitle("Corruption and Human Development")
-
+            check_overlap = TRUE, alpha=1) +
+  #Add title
+  ggtitle("Corruption and Human Development") +
+  #add theme and center title
+  theme_bw() + theme(plot.title = element_text(hjust=0.5)) 
 
 #Label Points to show on chart
 label.points <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
@@ -39,5 +35,5 @@ label.points <- c("Russia", "Venezuela", "Iraq", "Myanmar", "Sudan",
                   "India", "Italy", "China", "South Africa", "Spane",
                   "Botswana", "Cape Verde", "Bhutan", "Rwanda", "France",
                   "United States", "Germany", "Britain", "Barbados", "Norway", "Japan",
-                  "New Zealand", "Singapore", "Syria", "Japan")
+                  "New Zealand", "Singapore", "Syria")
 
